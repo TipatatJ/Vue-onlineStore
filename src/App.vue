79283da86fce7fac768be33e8342d6d1077a3f27
iamEvans <711hikaru@gmail.com>
2018-11-13 22:45:42 +0800

    Vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created(){
+    const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    this.$http.get(api).then((response) => {
+
+    })
+  },
+}
+</script>
+
+<style lang="scss">
+@import './assets/all';
+
+body {
+  background: linear-gradient(rgba(255,255,255,0.85),rgba(255,255,255,0.85)),url(./assets/bodyBcg.jpeg);
+}
+
+ul,li {
+  list-style-type: none;
+}
+
+</style>
---
+++ src/components/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <navbar></navbar>
+        <store></store>
+        <router-view></router-view>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import Navbar from './front-end/navbar.vue'
+import Store from './front-end/store.vue'
+import Footer from './front-end/footer.vue'
+
+    export default {
+        components: {
+            'navbar': Navbar,
+            'store': Store,
+            'app-footer':Footer
+        }
+    }
+</script>
+
+
+<style lang="sass" scoped>
+
+</style>
